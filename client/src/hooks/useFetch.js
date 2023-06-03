44b5aceadc519e9934c1d44359bc37c89e4464b5
Huy <s3825059@rmit.edu.vn>
2023-06-03 13:28:11 +0700

    remove cache from FE code and re-commit

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const useFetch = (url) => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      setLoading(true);
+
+      try {
+        const { data } = await axios.get(url);
+        setData(data);
+      } catch (error) {
+        setError(error);
+      }
+      setLoading(false);
+    };
+    fetchApi();
+  }, [url]);
+
+  const refetch = async () => {
+    setLoading(true);
+
+    try {
+      const { data } = await axios.get(url);
+      setData(data);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, refetch };
+};
